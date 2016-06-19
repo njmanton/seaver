1c450d3713a9fdd8a1c155dc2e349696f5cd90cd
Nick Manton <nick@mantonbradbury.com>
2016-06-19 09:58:01 +0100

    change week calculation to ensure standings shown on front page

--- routes.js
+++ routes.js
@@ -16,7 +16,7 @@ module.exports = function(app) {
   app.get('/', function(req, res) {
     let start = moment('2016 06 27', 'YYYY MM DD'),
         now = moment();
-    var week = (Math.floor(now.diff(start, 'days') / 7) + 1) || 1;
+    var week = (Math.floor(now.diff(start, 'days') / 7) + 1) || -1;
     models.Match.fixtures(models, week, 2016).then(function(data) {
       if (data.length) {
         res.render('weeks', {
