aac1baaab51a0d395198dc624dcebcbc4e523734
Nick Manton <nick@mantonbradbury.com>
2017-03-22 13:12:54 +0000

    add stub for email processing; ES6 changes

--- routes.js
+++ routes.js
@@ -1,34 +1,36 @@
+//jshint node: true, esversion: 6
 'use strict';
 
 var models    = require('./models'),
     moment    = require('moment'),
+    config    = require('./config'),
     passport  = require('passport');
 
-module.exports = function(app) {
+const routes = app => {
 
-  var isAuthenticated = function (req, res, next) {
+  var isAuthenticated = (req, res, next) => {
     if (req.isAuthenticated())
       return next();
     res.redirect('/login');
   }
 
   // home page
-  app.get('/', function(req, res) {
+  app.get('/', (req, res) => {
     let start = moment('2016 06 27', 'YYYY MM DD'),
         now = moment();
     var week = (Math.floor(now.diff(start, 'days') / 7) + 10) || -1;
-    models.Match.fixtures(models, week, 2016).then(function(data) {
+    models.Match.fixtures(models, week, config.year).then(data => {
       if (data.length) {
         res.render('weeks', {
           title: 'This Week\'s Games',
           weeks: data
         })
       } else {
-        models.Match.table(models, 2016).then(table => { 
+        models.Match.table(models, config.year).then(table => { 
           res.render('standings', {
             title: 'Standings',
             league: table,
-            season: 2016
+            season: config.year
           })
         });
       }
@@ -38,7 +40,7 @@ module.exports = function(app) {
 
   // login
   app.route('/login')
-    .get(function(req, res) {
+    .get((req, res) => {
       res.render('login', {
         title: 'Login'
       })
@@ -49,20 +51,20 @@ module.exports = function(app) {
       })
     );
 
-  app.get('/logout', isAuthenticated, function(req, res) {
+  app.get('/logout', isAuthenticated, (req, res) => {
     req.logout();
     req.flash('message', 'User Logged Off');
     res.redirect('/');
   })
 
   // static pages
-  app.get('/rules', function(req, res) {
+  app.get('/rules', (req, res) => {
     res.render('rules', {
       title: 'Rules'
     })
   })
 
-  app.get('/format', function(req, res) {
+  app.get('/format', (req, res) => {
     res.render('format', {
       title: 'League Format'
     })
@@ -70,13 +72,13 @@ module.exports = function(app) {
 
   app.get('/unplayed', (req, res) => {
     
-    let start = moment('2016 04 11', 'YYYY MM DD'),
+    let start = moment(config.first_week, 'YYYY MM DD'),
         now = moment();
     var week = (Math.floor(now.diff(start, 'days') / 7) + 1) || 1;
     models.Match.findAll({
       order: ['round'],
       attributes: ['id', 'round', 'round', 'score'],
-      where: [{ score: null }, { round: { $lte: week } }, { season: 2016 }],
+      where: [{ score: null }, { round: { $lte: week } }, { season: config.year }],
       include: [{
         model: models.Team,
         as: 'TeamA',
@@ -100,21 +102,22 @@ module.exports = function(app) {
 
   // get the standings
   // optionally add a year to get that year's table
-  app.get('/standings/:season?', function(req, res) {
-    models.Match.table(models, req.params.season).then(function(table) { 
+  app.get('/standings/:season?', (req, res) => {
+    models.Match.table(models, req.params.season).then(table => { 
       res.render('standings', {
         title: 'Standings',
         league: table,
-        season: req.params.season || 2016
+        season: req.params.season || config.year
       })
     });
   })
 
   // get a team's result
-  app.get('/teams/:id/:season?', function(req, res) {
+  app.get('/teams/:id/:season?', (req, res) => {
     var team = models.Team.findById(req.params.id);
     var matches = models.Match.findAll({
-      where: { $and: [{ season: req.params.season || app.locals.season }, { $or: [{ teama_id: req.params.id }, { teamb_id: req.params.id }] }] },
+      where: { $and: [{ season: req.params.season || app.locals.season }, 
+             { $or: [{ teama_id: req.params.id }, { teamb_id: req.params.id }] }] },
       raw: true,
       attributes: [
         'id',
@@ -134,44 +137,41 @@ module.exports = function(app) {
         attributes: ['id', 'name']
       }]    
     });
-    models.sequelize.Promise.join(
-      team,
-      matches,
-      function(team, matches) {
-        team.logo = team.name.replace(/\s|\'/g, '').toLowerCase() + '.png';
-        for (var x = 0; x < matches.length; x++) {
-          var scores = [], result = null;
-          if (matches[x].teama_id == req.params.id) {
-            matches[x].home = true;
-            matches[x].logo = matches[x]['TeamB.name'].replace(/\s|\'/g, '').toLowerCase() + '.png';
-          } else {
-            matches[x].home = false;
-            matches[x].logo = matches[x]['TeamA.name'].replace(/\s|\'/g, '').toLowerCase() + '.png';
-          }
-
-          if (matches[x].score) {
-            scores = matches[x].score.split('-').map(function(e) { return e * 1; });
-            if (matches[x].home) {
-              matches[x].cls = (scores[0] > scores[1]) ? 'win' : 'lose';
-            } else {
-              matches[x].cls = (scores[0] < scores[1]) ? 'win' : 'lose';
-              // if selected team is 'away' then reverse the order of the score
-              matches[x].score = matches[x].score.split('-').reverse().join('-');
-            }
-          } else {
-            matches[x].score = '-';
-          }
+    models.sequelize.Promise.join(team, matches, (team, matches) => {
+      team.logo = team.name.replace(/\s|\'/g, '').toLowerCase() + '.png';
+      for (var x = 0; x < matches.length; x++) {
+        var scores = [], result = null;
+        if (matches[x].teama_id == req.params.id) {
+          matches[x].home = true;
+          matches[x].logo = matches[x]['TeamB.name'].replace(/\s|\'/g, '').toLowerCase() + '.png';
+        } else {
+          matches[x].home = false;
+          matches[x].logo = matches[x]['TeamA.name'].replace(/\s|\'/g, '').toLowerCase() + '.png';
         }
-        res.render('teams', {
-          title: team.name,
-          team: team,
-          matches: matches
-        })
+
+        if (matches[x].score) {
+          scores = matches[x].score.split('-').map(e => { return e * 1; });
+          if (matches[x].home) {
+            matches[x].cls = (scores[0] > scores[1]) ? 'win' : 'lose';
+          } else {
+            matches[x].cls = (scores[0] < scores[1]) ? 'win' : 'lose';
+            // if selected team is 'away' then reverse the order of the score
+            matches[x].score = matches[x].score.split('-').reverse().join('-');
+          }
+        } else {
+          matches[x].score = '-';
+        }
+      }
+      res.render('teams', {
+        title: team.name,
+        team: team,
+        matches: matches
       })
+    })
   })
 
-  app.get('/weeks/:id?/:season?', function(req, res) {
-    models.Match.fixtures(models, req.params.id, req.params.season).then(function(data) {
+  app.get('/weeks/:id?/:season?', (req, res) => {
+    models.Match.fixtures(models, req.params.id, req.params.season).then(data => {
       res.render('weeks', {
         title: (req.params.id) ? 'LCSSL Week ' + req.params.id : 'LCSSL Fixtures',
         weeks: data
@@ -179,8 +179,17 @@ module.exports = function(app) {
     })
   })
 
+  // handle an email submitted to results@lcssl.org
+  app.post('/process_email', (req, res) => {
+    // parse the request for data, team, score, team
+    // if parsed correctly, save result and send email back
+    // otherwise send failure email
+    console.log(req);
+  });
+
+
   // handle the ajax POST of a result
-  app.post('/matches/result', function(req, res) {
+  app.post('/matches/result', (req, res) => {
     
     if (req.user) {
       if (req.body.score.match(/[0-9]{1,2}-[0-9]{1,2}/)) {
@@ -188,9 +197,9 @@ module.exports = function(app) {
           score: req.body.score || null
         }, {
           where: [{ id: req.body.mid }, { season: 2016 }]
-        }).then(function(rows) {
+        }).then(rows => {
           res.send(rows == 1);
-        }).catch(function(e) {
+        }).catch(e => {
           res.send(e);
         })
       } else {
@@ -202,4 +211,13 @@ module.exports = function(app) {
     }
   })
 
+  app.get('/dodgeball/rules', (req, res) => {
+    res.render('db_home', {
+      title: 'Rules',
+      layout: 'dodgeball'
+    });
+  })
+
 }
+
+module.exports = routes;
