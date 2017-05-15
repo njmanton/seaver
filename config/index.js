3f9ff64843309e693340a64a06d9a8956bb27596
Nick Manton <nick@mantonbradbury.com>
2017-05-15 09:59:49 +0100

    add config (now without credentials) to repo

---
+++ config/index.js
@@ -0,0 +1,26 @@
+//jshint node: true, esversion: 6
+'use strict';
+
+exports.year = 2017;
+exports.first_week = '2017 05 10';
+exports.teams = {
+  'weapons': 11,
+  'internationweapons': 11,
+  'transporters': 1,
+  'dft': 1,
+  'transport': 1,
+  'dcms': 2,
+  'dcmets': 2,
+  'sportengland': 3,
+  'darwinsdevils': 5,
+  'darwins': 5,
+  'devils': 5,
+  'mrc': 6,
+  'science': 7,
+  'sciencemuseum': 7,
+  'honeybadgers': 9,
+  'badgers': 9,
+  'cobisrangers': 16,
+  'cobis': 16,
+  'rangers': 16
+};
